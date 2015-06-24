c3d993db3e8bb8668f6c1d557af5564433d954de
Owens O'Brien <owensobrien@gmail.com>
2015-06-24 10:24:43 -0400

    blank files and deck object

---
+++ js/app.js
@@ -0,0 +1,3 @@
+console.log("App Loaded")
+//saves shuffled cards into a variable
+var shuffled = deck.sort(function() { return 0.5 - Math.random() });
---
+++ js/deck.js
@@ -0,0 +1,316 @@
+console.log("Deck Loaded")
+
+var deck = [
+	{
+	card: "Ace of Spades",
+	suit:"Spades",
+	value:11 || 1, ////////////////////
+	facedown:true
+	},
+	{
+	card:"King of Spades",
+	suit:"Spades",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Queen of Spades",
+	suit:"Spades",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Jack of Spades",
+	suit:"Spades",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Ten of Spades",
+	suit:"Spades",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Nine of Spades",
+	suit:"Spades",
+	value:9,
+	facedown:true
+	},
+	{
+	card:"Eight of Spades",
+	suit:"Spades",
+	value:8,
+	facedown:true
+	},
+	{
+	card:"Seven of Spades",
+	suit:"Spades",
+	value:7,
+	facedown:true
+	},
+	{
+	card:"Six of Spades",
+	suit:"Spades",
+	value:6,
+	facedown:true
+	},
+	{
+	card:"Five of Spades",
+	suit:"Spades",
+	value:5,
+	facedown:true
+	},
+	{
+	card: "Four of Spades",
+	suit:"Spades",
+	value:4,
+	facedown:true
+	},
+	{
+	card:"Three of Spades",
+	suit:"Spades",
+	value:3,
+	facedown:true
+	},
+	{
+	card:"Two of Spades",
+	suit:"Spades",
+	value:2,
+	facedown:true
+	},
+	{
+	card:"Ace of Clubs",
+	suit:"Clubs",
+	value:11 || 1,////////////////////////
+	facedown:true
+	},
+	{
+	card:"King of Clubs",
+	suit:"Clubs",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Queen of Clubs",
+	suit:"Clubs",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Jack of Clubs",
+	suit:"Clubs",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Ten of Clubs",
+	suit:"Clubs",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Nine of Clubs",
+	suit:"Clubs",
+	value:9,
+	facedown:true
+	},
+	{
+	card: "Eight of Clubs",
+	suit:"Clubs",
+	value:8,
+	facedown:true
+	},
+	{
+	card:"Seven of Clubs",
+	suit:"Clubs",
+	value:7,
+	facedown:true
+	},
+	{
+	card:"Six of Clubs",
+	suit:"Clubs",
+	value:6,
+	facedown:true
+	},
+	{
+	card:"Five of Clubs",
+	suit:"Clubs",
+	value:5,
+	facedown:true
+	},
+	{
+	card:"Four of Clubs",
+	suit:"Clubs",
+	value:4,
+	facedown:true
+	},
+	{
+	card:"Three of Clubs",
+	suit:"Clubs",
+	value:3,
+	facedown:true
+	},
+	{
+	card:"Two of Clubs",
+	suit:"Clubs",
+	value:2,
+	facedown:true
+	},
+	{
+	card:"Ace of Hearts",
+	suit:"Hearts",
+	value:11 || 1,/////////////////////////////
+	facedown:true
+	},
+	{
+	card:"King of Hearts",
+	suit:"Hearts",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Queen of Hearts",
+	suit:"Hearts",
+	value:10,
+	facedown:true
+	},
+	{
+	card: "Jack of Hearts",
+	suit:"Hearts",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Ten of Hearts",
+	suit:"Hearts",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Nine of Hearts",
+	suit:"Hearts",
+	value:9,
+	facedown:true
+	},
+	{
+	card:"Eight of Hearts",
+	suit:"Hearts",
+	value:8,
+	facedown:true
+	},
+	{
+	card:"Seven of Hearts",
+	suit:"Hearts",
+	value:7,
+	facedown:true
+	},
+	{
+	card:"Six of Hearts",
+	suit:"Hearts",
+	value:6,
+	facedown:true
+	},
+	{
+	card:"Five of Hearts",
+	suit:"Hearts",
+	value:5,
+	facedown:true
+	},
+	{
+	card:"Four of Hearts",
+	suit:"Hearts",
+	value:4,
+	facedown:true
+	},
+	{
+	card:"Three of Hearts",
+	suit:"Hearts",
+	value:3,
+	facedown:true
+	},
+	{
+	card:"Two of Hearts",
+	suit:"Hearts",
+	value:2,
+	facedown:true
+	},
+	{
+	card: "Ace of Diamonds",
+	suit:"Diamonds",
+	value:11 || 1,///////////////////////////
+	facedown:true
+	},
+	{
+	card:"King of Diamonds",
+	suit:"Diamonds",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Queen of Diamonds",
+	suit:"Diamonds",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Jack of Diamonds",
+	suit:"Diamonds",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Ten of Diamonds",
+	suit:"Diamonds",
+	value:10,
+	facedown:true
+	},
+	{
+	card:"Nine of Diamonds",
+	suit:"Diamonds",
+	value:9,
+	facedown:true
+	},
+	{
+	card:"Eight of Diamonds",
+	suit:"Diamonds",
+	value:8,
+	facedown:true
+	},
+	{
+	card:"Seven of Diamonds",
+	suit:"Diamonds",
+	value:7,
+	facedown:true
+	},
+	{
+	card:"Six of Diamonds",
+	suit:"Diamonds",
+	value:6,
+	facedown:true
+	},
+	{
+	card:"Five of Diamonds",
+	suit:"Diamonds",
+	value:5,
+	facedown:true
+	},
+	{
+	card:"Four of Diamonds",
+	suit:"Diamonds",
+	value:4,
+	facedown:true
+	},
+	{
+	card:"Three of Diamonds",
+	suit:"Diamonds",
+	value:3,
+	facedown:true
+	},
+	{
+	card:"Two of Diamonds",
+	suit:"Diamonds",
+	value:2,
+	facedown:true
+	}
+]
